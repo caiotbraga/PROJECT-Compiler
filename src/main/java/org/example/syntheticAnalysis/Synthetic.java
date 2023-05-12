554105605952da6c525166d03a1da2f23cd2a8c4
Caio Braga <caiotabraga@hotmail.com>
2023-05-12 13:56:55 -0300

    Created Synthetic class

---
+++ src/main/java/org/example/syntheticAnalysis/Synthetic.java
@@ -0,0 +1,14 @@
+package org.example.syntheticAnalysis;
+
+import org.example.lexicalAnalysis.Lexicon;
+import org.example.lexicalAnalysis.Token;
+
+public class Synthetic {
+    private Lexicon lexicon;
+    private Token token;
+
+    public Synthetic(Lexicon lex){
+        this.lexicon = lex;
+    }
+    
+}
