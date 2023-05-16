25b10720dab21cf6bb91459336e9403189ebdf48
Caio Braga <caiotabraga@hotmail.com>
2023-05-16 16:54:36 -0300

    Implementing GLC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,7 @@ import org.example.syntheticAnalysis.*;
 public class Main {
     public static void main(String[] args) {
         Lexicon lexicon = new Lexicon("C:\\Users\\11159819432\\Desktop\\COMP\\Compiler\\src\\main\\java\\org\\example\\lexicalAnalysis\\code.txt");
-        Token t = null;
-        while ((t = lexicon.nextToken()) != null) {
-            System.out.println(t.toString());
-        }
+        Synthetic synthetic = new Synthetic(lexicon);
+        synthetic.S();
     }
 }
