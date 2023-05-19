66021337f8e6dd4ce7ec4ebeb4fc2a83ce4a0088
Caio Braga <caiotabraga@hotmail.com>
2023-05-19 10:05:45 -0300

    Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.example.lexicalAnalysis.*;
 import org.example.syntheticAnalysis.*;
 public class Main {
     public static void main(String[] args) {
-        Lexicon lexicon = new Lexicon("C:\\Users\\11159819432\\Desktop\\COMP\\Compiler\\src\\main\\java\\org\\example\\lexicalAnalysis\\code.txt");
+        Lexicon lexicon = new Lexicon("/Users/caiotbraga/Desktop/Projetos/Compiler/Compiler/src/main/java/org/example/code.txt");
         Synthetic synthetic = new Synthetic(lexicon);
         synthetic.S();
     }
